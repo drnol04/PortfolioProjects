145e215721582063c46e5c75c02aa25e18313ead
Diego Nolasco <165108120+drnol04@users.noreply.github.com>
2024-04-03 16:38:05 -0600

    Way to create caesar-cipher-1-total.py

--- caesar-cipher-1-total.py
+++ caesar-cipher-1-total.py
@@ -13,9 +13,25 @@ def caesar(start_text, shift_amount, cipher_direction):
             end_text += char
     print(f"Here's the {direction}d result: {end_text}")
 
-from art import logo
-print(logo)
-
+# from art import logo
+# print(logo)
+# logo = """           
+#  ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+# a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+# 8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+# "8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+#  `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+#             88             88                                 
+#            ""             88                                 
+#                           88                                 
+#  ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+# a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+# 8b         88 88       d8 88       88 8PP""""""" 88          
+# "8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+#  `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+#               88                                             
+#               88           
+# """
 should_end = False
 while not should_end:
 
